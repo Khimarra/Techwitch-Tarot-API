9957e77f4b1238aeb3c44e07c0a7a88222125a89
khimarra <tsolbrig87@gmail.com>
2020-04-30 22:40:16 -0400

    built initial routes

--- controllers/index.js
+++ controllers/index.js
@@ -22,7 +22,58 @@ const getAllCards = async (req, res) => {
 const getSuitById = async (req, res) => {
     try {
         const { id } = req.params
-        
+        const suit = await Suit.findOne({
+            where: { id: id }
+        })
+        if (suit) {
+            return res.status(200).json({ suit })
+        }
+        return res.status(404).send('That suit does not exist.')
+    } catch (err) {
+        return res.status(500).send(err.message)
+    }
+}
+
+const getCardById = async (req, res) => {
+    try {
+        const { id } = req.params
+        const card = await Card.findOne({
+            where: { id: id }
+        })
+        if (card) {
+            return res.status(200).json({ card })
+        }
+    } catch (err) {
+        return res.status(500).sned(err.message)
+    }
+}
+
+const getAllCardsBySuit = async (req, res) => {
+    try {
+        const suit = await Suit.findByPk(rec.params.id, {
+            include: [
+                { model: card }
+            ]
+        })
+        return res.status(200).json({ suit })
+    } catch (err) {
+        return res.status(500).send(err.message)
+    }
+}
+
+const getAllCardsByValue = async (req, res) => {
+    try {
+        const { value } = req.params
+        const card = await Card.findAll({
+            where: {
+                value: value
+            }
+        })
+        if (card) {
+            return res.status(200).json({ card })
+        }
+    } catch (err) {
+        return res.status()
     }
 }
 
